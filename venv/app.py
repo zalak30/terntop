cd8d70b817681da65b7756e1783c61013aec68f8
Zalak <zalakpatel534@gmail.com>
2020-09-03 21:30:24 +0530

    create models and CRUD operations

--- venv/app.py
+++ venv/app.py
@@ -1,57 +1,20 @@
 from flask import Flask
 from flask import request
 from conn import Connection
+from models.intern import Interns
 
 app = Flask(__name__)
 
 
 # api for user information.
-@app.route('/', methods=['GET', 'POST'])
-def create_cm():
-    if request.method == 'POST':
-        obj = Connection()
+@app.route('/user/<user_id>', methods=['GET', 'POST', 'PUT'])
+def personal_info(user_id):
+    if request.method == 'PUT':
         content = request.get_json()
-        print(content)
-        obj.connect_database()
-        First_name = content['first_name']
-        Last_name = content['last_name']
-        Full_name = First_name + ' ' + Last_name
-        Image_url = content['image_url']
-        Phone = content['phone']
-        Country_code = content['country_code']
-        City = content['city']
-        State = content['state']
-        Country = content['country']
+        return Interns.update_intern(user_id, content)
 
-        query = "INSERT INTO interns(first_name, last_name, full_name, image_url, " \
-                "phone, country_code, city, state, country) " \
-                "VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)"
-
-        values = (First_name, Last_name, Full_name, Image_url, Phone, Country_code, City, State, Country)
-
-        obj.cursor.execute(query, values)
-        obj.conn.commit()
-        obj.close()
-        return {'status': 200, 'message': 'User registered successfully..'}
-
-
-
-        # elif request.method == 'GET':
-        #     obj = Connection()
-        #     obj.cursor.execute("USE terntop")
-        #     uid = request.args.get('uid')
-        #     obj.cursor.execute("SELECT first_name, last_name FROM interns WHERE id = {}".format(uid))
-        #     user = obj.cursor.fetchone()
-        #     print(user)
-        #     row_header = [x[0] for x in obj.cursor.description]
-        #     print(row_header)
-        #     obj.conn.commit()
-        #     obj.close()
-        #
-        #     if user is not None:
-        #         return {'status': 200, 'data': dict(zip(row_header, user))}
-        #     else:
-        #         return 'user not found'
+    elif request.method == 'GET':
+        return Interns.get_intern(user_id)
 
 
 app.run(debug=True)
--- venv/create_database.py
+++ venv/create_database.py
@@ -16,8 +16,8 @@ obj.connect_database()
 
 # create table "interns"
 try:
-    obj.cursor.execute("CREATE TABLE interns (id INT AUTO_INCREMENT PRIMARY KEY, pw_hash VARCHAR(100),"
-                       "first_name VARCHAR(10), last_name VARCHAR(10), "
+    obj.cursor.execute("CREATE TABLE interns (id INT AUTO_INCREMENT PRIMARY KEY,user_id VARCHAR(10) NOT NULL UNIQUE,"
+                       " pw_hash VARCHAR(100), first_name VARCHAR(10), last_name VARCHAR(10), "
                        "full_name VARCHAR(20), image_url TEXT,phone BIGINT(10), country_code VARCHAR(2),"
                        "city VARCHAR(20), state VARCHAR(20), country VARCHAR(20), "
                        "date_registered TIMESTAMP, date_updated TIMESTAMP,"
@@ -28,9 +28,9 @@ except Exception as e:
 
 # create table "employers"
 try:
-    obj.cursor.execute("CREATE TABLE employers (id INT AUTO_INCREMENT PRIMARY KEY, pw_hash VARCHAR(100),"
-                       "first_name VARCHAR(10), last_name VARCHAR(10), full_name VARCHAR(20), image_url TEXT,"
-                       "phone INT(10), country_code VARCHAR(2), city VARCHAR(20), state VARCHAR(20),"
+    obj.cursor.execute("CREATE TABLE employers (id INT AUTO_INCREMENT PRIMARY KEY,user_id VARCHAR(10) NOT NULL UNIQUE," 
+                       " pw_hash VARCHAR(100), first_name VARCHAR(10), last_name VARCHAR(10), full_name VARCHAR(20),"
+                       " image_url TEXT, phone INT(10), country_code VARCHAR(2), city VARCHAR(20), state VARCHAR(20),"
                        "country VARCHAR(20), date_registered TIMESTAMP, date_updated TIMESTAMP,"
                        "company_name VARCHAR(40), company_website TEXT, company_about TEXT, jobs TEXT)")
     print('"employers" table created successfully')
