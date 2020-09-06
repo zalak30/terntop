f9ac946d62af75225f75b375db4fb17900d9a34d
Zalak <zalakpatel534@gmail.com>
2020-09-06 23:18:35 +0530

    rest api

--- venv/app.py
+++ venv/app.py
@@ -7,24 +7,29 @@ app = Flask(__name__)
 
 # api for intern information.
 @app.route('/intern/<user_id>', methods=['GET', 'PUT', 'DELETE'])
+@app.route('/intern/', defaults={'user_id': None}, methods=['POST'])
 def personal_info(user_id):
-    if request.method == 'PUT':
-        content = request.get_json()
-        return Interns.update_intern(user_id, content)
-
-    elif request.method == 'GET':
-        return Interns.get_intern(user_id)
-
-    else:
-        return Interns.delete_intern(user_id)
-
-
-# api for intern registration.
-@app.route('/intern', methods=['POST'])
-def register_info():
+    # register intern
     if request.method == 'POST':
         content = request.get_json()
-        return Interns.register_intern(content)
+        interns = Interns()
+        return interns.register_intern(content)
+
+    # update intern info using user_id
+    elif request.method == 'PUT':
+        content = request.get_json()
+        interns = Interns()
+        return interns.update_intern(user_id, content)
+
+    # get intern info using user_id
+    elif request.method == 'GET':
+        interns = Interns()
+        return interns.get_intern(user_id)
+
+    # delete intern using user_id
+    else:
+        interns = Interns()
+        return interns.delete_intern(user_id)
 
 
 app.run(debug=True)
