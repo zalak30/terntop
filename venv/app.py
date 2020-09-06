89bb4a3247590c35b6d8725a05b4d6247b75d760
Zalak <zalakpatel534@gmail.com>
2020-09-06 16:28:48 +0530

    CRUD operations successfully

--- venv/app.py
+++ venv/app.py
@@ -1,13 +1,12 @@
 from flask import Flask
 from flask import request
-from conn import Connection
-from models.intern import Interns
+from models.interns import Interns
 
 app = Flask(__name__)
 
 
-# api for user information.
-@app.route('/user/<user_id>', methods=['GET', 'POST', 'PUT'])
+# api for intern information.
+@app.route('/intern/<user_id>', methods=['GET', 'PUT', 'DELETE'])
 def personal_info(user_id):
     if request.method == 'PUT':
         content = request.get_json()
@@ -15,6 +14,17 @@ def personal_info(user_id):
 
     elif request.method == 'GET':
         return Interns.get_intern(user_id)
+
+    else:
+        return Interns.delete_intern(user_id)
+
+
+# api for intern registration.
+@app.route('/intern', methods=['POST'])
+def register_info():
+    if request.method == 'POST':
+        content = request.get_json()
+        return Interns.register_intern(content)
 
 
 app.run(debug=True)
